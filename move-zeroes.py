75c1d0ef20d54b8f5abc77824b051563d9bdc7e1
Courser <wangzhongdi@meituan.com>
2020-07-09 20:02:16 +0800

    feature:init move-zeros.py

---
+++ move-zeroes.py
@@ -0,0 +1,13 @@
+# https://leetcode-cn.com/problems/move-zeroes/
+from typing import List
+
+
+class Solution:
+
+    def moveZeroes(self, nums: List[int]) -> None:
+
+        return None
+
+    def quickSort(self, nums: List[int], left: int, right: int) -> List[int]:
+
+        return []
