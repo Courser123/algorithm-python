5df16c4cb164cdabac53607d4b7cb64570234c4b
Courser <wangzhongdi@meituan.com>
2020-07-10 18:20:51 +0800

    feature:update

--- move-zeroes.py
+++ move-zeroes.py
@@ -10,4 +10,35 @@ class Solution:
 
     def quickSort(self, nums: List[int], left: int, right: int) -> List[int]:
 
-        return []
+        if (left < right):
+
+            baseNum = nums[left]
+            begin = left
+            end = right
+
+            while begin < end:
+
+                if nums[begin] < baseNum:
+                    begin += 1
+
+                elif nums[end] > baseNum:
+                    end -= 1
+
+                else:
+                    temp = nums[begin]
+                    nums[begin] = nums[end]
+                    nums[end] = temp
+
+            temp = baseNum
+            baseNum = nums[end]
+            nums[end] = temp
+
+            self.quickSort(nums, left, begin - 1)
+            self.quickSort(nums, begin + 1, right)
+
+        return nums
+
+
+test = Solution()
+
+test.quickSort([0, 1, 0, 3, 12], 0, 4)
